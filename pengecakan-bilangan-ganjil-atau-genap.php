98f226c1a7603a0bf0d88abe6937cc11073c4c63
Navalianto Dwi Ramadhan <108710536+navaldr@users.noreply.github.com>
2023-03-27 17:25:25 +0700

    Add files via upload

---
+++ pengecakan-bilangan-ganjil-atau-genap.php
@@ -0,0 +1,9 @@
+<?php
+  $angka = 1; // ganti dengan angka yang ingin diperiksa
+  
+  if($angka % 2 == 0){
+    echo $angka . " adalah bilangan genap";
+  } else{
+    echo $angka . " adalah bilangan ganjil";
+  }
+?>
