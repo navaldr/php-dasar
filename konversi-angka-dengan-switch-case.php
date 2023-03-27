76a80c33143b453429c03f222bc6888600ff88ed
Navalianto Dwi Ramadhan <108710536+navaldr@users.noreply.github.com>
2023-03-27 17:29:28 +0700

    Add files via upload
    Pada contoh di atas, angka yang akan dikonversi disimpan dalam variabel $angka. Kemudian, struktur kontrol switch-case digunakan untuk mengkonversi angka tersebut menjadi huruf. Setiap kasus (case) dalam switch berisi sebuah kondisi yang akan diuji terhadap variabel $angka. Jika nilai $angka sama dengan nilai dalam kasus, maka blok kode yang terdapat pada kasus tersebut akan dieksekusi. Kondisi default dijalankan jika tidak ada kasus yang sesuai dengan nilai $angka.

---
+++ konversi-angka-dengan-switch-case.php
@@ -0,0 +1,25 @@
+<?php
+// Mengatur angka yang akan dikonversi
+$angka = 3;
+
+// Konversi angka ke dalam huruf
+switch ($angka) {
+  case 1:
+    echo "Satu";
+    break;
+  case 2:
+    echo "Dua";
+    break;
+  case 3:
+    echo "Tiga";
+    break;
+  case 4:
+    echo "Empat";
+    break;
+  case 5:
+    echo "Lima";
+    break;
+  default:
+    echo "Angka tidak dikenali";
+}
+?>
